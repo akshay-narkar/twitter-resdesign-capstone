a36b5d821935c5f7d8a8b2b4cc1b22670cc70484
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-18 20:17:47 +0530

    Set up login using sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.find_by_username(params_login[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: 'You have successfully logged in'
+    else
+      redirect_to new_session_path, notice: 'Invalid Username'
+    end
+  end
+
+  def show; end
+
+  def index; end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'Successfully Logged Out'
+  end
+
+  private
+
+  def params_login
+    params.require(:@userlogin).permit(:username)
+  end
+end
