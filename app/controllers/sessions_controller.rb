868f9af458139d2afa0cd3997544774903ec28ab
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-21 14:21:55 +0530

    Change redirection to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_username(params_login[:username])
     if @user
       session[:user_id] = @user.id
-      redirect_to user_path(@user.id), notice: 'You have successfully logged in'
+      redirect_to root_path, notice: 'You have successfully logged in'
     else
       redirect_to new_session_path, notice: 'Invalid Username'
     end
