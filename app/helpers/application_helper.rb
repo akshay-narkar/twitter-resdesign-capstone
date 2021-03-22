d35ec19783be1456bee8bd7483138953b17d9eac
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-23 02:31:42 +0530

    Update application helper with checkiffollowed method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,45 @@
 module ApplicationHelper
-  
   def checklogin
     if session[:user_id].present?
-      render 'layouts/template' 
-    
-  else
-     render 'layouts/notloggedintemplate'
-  end
+      render 'layouts/template'
+
+    else
+      render 'layouts/notloggedintemplate'
+    end
   end
 
   def notice1
-    if notice 
+    if notice
       render 'layouts/notice'
     elsif alert
       render 'layouts/alert'
+    end
   end
-end
+
+  def checkiffollowed(tweet, following)
+    return unless following.include?(tweet.author_id)
+
+    diff = Time.diff(tweet.created_at, Time.now)
+    diff1 = timedifference(diff)
+
+    render partial: 'tweets/followertweets', locals: { tweet: tweet, diff: diff1 }
+  end
+
+  def timedifference(diff)
+    if diff[:year] > 1
+      "#{diff[:year]} Years ago"
+    elsif diff[:month] > 1
+      "#{diff[:month]} months ago"
+    elsif diff[:week] > 1
+      "#{diff[:week]} weeks ago"
+    elsif diff[:day] > 1
+      "#{diff[:day]} days ago"
+    elsif diff[:day] == 1
+      "#{diff[:day]} day ago"
+    elsif diff[:hours] >= 1
+      "#{diff[:day]} Hours ago"
+    else
+      "#{diff[:minutes]} Minutes ago"
+    end
+  end
 end
