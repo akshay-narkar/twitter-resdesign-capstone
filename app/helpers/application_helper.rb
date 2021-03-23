c3df13bf77721f845b3d34e0bbc39115d69e3537
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-23 21:12:41 +0530

    Chnage checkiffollowed conditions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,7 +17,6 @@ module ApplicationHelper
   end
 
   def checkiffollowed(tweet, following)
-
     return unless following.include?(tweet.author_id)
 
     diff = Time.diff(tweet.created_at, Time.now)
@@ -26,20 +25,20 @@ module ApplicationHelper
     render partial: 'tweets/followertweets', locals: { tweet: tweet, diff: diff1 }
   end
 
-  def checkiffollowing(user,following)
+  def checkiffollowing(user, following)
     return if following.include?(user.id) || user.id == @user.id
 
     render partial: 'tweets/whotofollow', locals: { notfollower: user }
   end
 
   def timedifference(diff)
-    if diff[:year] > 1 
+    if diff[:year] > 1
       "#{diff[:year]} Years ago"
-    elsif diff[:month] > 1 
+    elsif diff[:month] > 1
       "#{diff[:month]} months ago"
-    elsif diff[:week] > 1 
+    elsif diff[:week] > 1
       "#{diff[:week]} weeks ago"
-    elsif diff[:day] > 1 
+    elsif diff[:day] > 1
       "#{diff[:day]} days ago"
     elsif diff[:day] == 1
       "#{diff[:day]} day ago"
@@ -50,22 +49,21 @@ module ApplicationHelper
     end
   end
 
-  def userphoto(user)
-   if @user.photo.attached?
-      image_tag @user.photo, class: "img-thumbnail" ,style: "width: 75px; height: 75px"
-   
+  def userphoto(_user)
+    if @user.photo.attached?
+      image_tag @user.photo, class: 'img-thumbnail', style: 'width: 75px; height: 75px'
 
-  else
-      image_tag("icon.jpeg", size: "75x75", class: "img-thumbnail")
-  end
+    else
+      image_tag('icon.jpeg', size: '75x75', class: 'img-thumbnail')
+    end
   end
 
   def followerphoto(notfollower)
-   if notfollower.photo.attached?
-      image_tag notfollower.photo, class: "rounded-circle" ,style: "width: 65px; height: 65px"
-   
-  else
-    image_tag("icon.jpeg", size: "65x65", class: "rounded-circle")
+    if notfollower.photo.attached?
+      image_tag notfollower.photo, class: 'rounded-circle', style: 'width: 65px; height: 65px'
+
+    else
+      image_tag('icon.jpeg', size: '65x65', class: 'rounded-circle')
+    end
   end
-end
 end
