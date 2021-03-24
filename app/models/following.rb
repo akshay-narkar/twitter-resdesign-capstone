d19e653866e9b6d876f1cc973633b83e23880e67
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-24 16:44:10 +0530

    Change uniqueness scope to _id

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,5 +1,5 @@
 class Following < ApplicationRecord
   belongs_to :follower, class_name: :User
   belongs_to :followed, class_name: :User
-  validates_uniqueness_of :follower, scope: %i[followed]
+  validates_uniqueness_of :follower_id, scope: %i[followed_id]
 end
