88ed8512f23dd9d13c9b1c743f22ed67fcae1d4d
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-24 16:44:35 +0530

    Change length message for tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ApplicationRecord
   belongs_to :author, class_name: :User
-  validates :text, presence: { message: 'Needs to be present' },
+  validates :text, presence: { message: 'Tweet cant be blank' },
                    length: { maximum: 280, message: 'Tweet limit is 280 characters' }
 end
