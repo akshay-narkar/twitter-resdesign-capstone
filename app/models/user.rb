33cc273b763ed94ce8241ebba7a3d01518c3baa5
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-24 16:45:04 +0530

    Remove compulsory presence of images

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   has_many :tweets, foreign_key: 'author_id', class_name: :Tweet, dependent: :destroy
   has_one_attached :photo, dependent: :destroy
   has_one_attached :coverimage, dependent: :destroy
-  validates :username, presence: { message: 'Needs to be present' },
+  validates :username, presence: { message: 'Username needs to be present' },
                        uniqueness: { message: 'Username already taken. Please try a new one' },
                        length: { minimum: 3, message: 'Username should be atleast 3 characters long' }
-  validates :fullname, presence: { message: 'Needs to be present' },
-                       length: { minimum: 3, message: 'Username should be atleast 3 characters long' }
+  validates :fullname, presence: { message: 'Fullname needs to be present' },
+                       length: { minimum: 3, message: 'Fullname should be atleast 3 characters long' }
 
-  validates :photo, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
-                    size: { less_than: 500.kilobytes, message: 'size needs to be lesser than 500 kbs' }
-  validates :coverimage, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
-                         size: { less_than: 500.kilobytes, message: 'size needs to be lesser than 500 kbs' }
+  validates :photo, content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+                    size: { less_than: 500.kilobytes, message: 'Profile Picture size needs to be lesser than 500 kbs' }
+  validates :coverimage,  content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+                         size: { less_than: 500.kilobytes, message: 'Coverimage size needs to be lesser than 500 kbs' }
 end
