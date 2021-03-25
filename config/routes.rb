cb03940831a167cfdc0f260234c942cd5cefaefb
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-25 20:49:32 +0530

    Update new action in users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tweets#new'
-  resources :users, only: [:show, :create, :show]
+  resources :users, only: [:show, :create, :show, :new]
   resources :tweets, only: [:new, :create]
   resources :sessions, only: [:new, :create,:destroy]
   resources :followings, only: [:new,:create,:destroy]
