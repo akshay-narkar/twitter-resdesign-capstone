3273b399c49e2791edaf2bf758f82203f8f1b94b
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-25 18:27:18 +0530

    Remove unecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tweets#new'
-  resources :users
-  resources :tweets
-  resources :sessions
-  resources :followings
+  resources :users, only: [:show, :create, :show]
+  resources :tweets, only: [:new, :create]
+  resources :sessions, only: [:new, :create,:destroy]
+  resources :followings, only: [:new,:create,:destroy]
 end
