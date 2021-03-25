51d0626f74084a971653e26b76b1df7e4a7746e0
akshay-narkar <akshaynarkar1295@gmail.com>
2021-03-25 21:16:18 +0530

    Redirect to root path after creating tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,7 @@ class TweetsController < ApplicationController
     @tweet = @user.tweets.build(tweet_params)
 
     if @tweet.save
-      redirect_to user_path(@user.id), method: :show, notice: 'Tweet successfully created'
+      redirect_to root_path, method: :show, notice: 'Tweet successfully created'
     else
       redirect_to new_tweet_path, notice: 'Failed to create tweet. Try again'
     end
